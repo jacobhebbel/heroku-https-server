04b34f60c2a6a447f3f1b45f843b179611ad131e
jacobhebbel <148645437+jacobhebbel@users.noreply.github.com>
2025-06-05 11:16:50 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,22 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-authCredentials = None
+authURL = None
 
 @app.route('/auth')
 def storeAuth():
-    global authCredentials
+    global authURL
     code = request.args.get('code')
-    state = request.args.get('state')
-
     if code:
-        authCredentials = {'ok': True, 'code': code, 'state': state}
+        authURL = request.url
         return 'Authorization received', 200
     else:
         return 'Failed to save auth', 400
 
 @app.route('/get')
 def retrieveUrl():
-    global authCredentials
-    if authCredentials:
-        data = authCredentials
-        authCredentials = None
-        return jsonify(data), 200
+    global authURL
+    if authURL:
+        return jsonify('url': authURL), 200
     else:
         return jsonify({'error': 'No auth URL yet'}), 404
