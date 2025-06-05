a6c1ae9ce4f4a860ee0dbff54485de76c7f0577a
jacobhebbel <148645437+jacobhebbel@users.noreply.github.com>
2025-06-05 10:30:04 -0400

    Create server.py
    server applications can communicate with

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+authURL = None
+
+@app.route('/auth')
+def storeUrl():
+    global authURL
+    authURL = request.url
+    return '', 200
+
+@app.route('/get')
+def retrieveUrl():
+    global authURL
+    if authURL:
+        url = authURL
+        authURL = None
+        return jsonify({'data': url}), 200
+    else:
+        return jsonify({'error': 'No auth URL yet'}), 401
